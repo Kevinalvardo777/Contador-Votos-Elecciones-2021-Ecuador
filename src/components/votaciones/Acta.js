d5bad9f3b722f6e28a398f8a719c3172e195c382
kevin <kevinalvapearson777@gmail.com>
2020-09-28 02:15:47 -0500

    hover para enlace-cuenta-acta

--- src/components/votaciones/Acta.js
+++ src/components/votaciones/Acta.js
@@ -5,13 +5,13 @@ const Acta = () => {
     return ( 
         <div className="container">
             <div className="jumbotron">
-                <h1 htmlFor="votaciones" className="text-center">Actas</h1>
+                <h1 htmlFor="votaciones" className="text-center" >Actas</h1>
             
             </div>
             <div className="row">
                 <div className="col-6 mx-auto">
                     <div className="listaProp">
-                            <Link to={"/presidentes"} className="enlace-cuenta">
+                            <Link to={"/presidentes"} className="enlace-cuenta-acta" >
                                 Presidentes
                             </Link>
                     </div>
@@ -20,7 +20,7 @@ const Acta = () => {
            <div className="row">
                 <div className="col-6 mx-auto">
                     <div className="listaProp">
-                            <Link to={"/presidentes"} className="enlace-cuenta">
+                            <Link to={"/presidentes"} className="enlace-cuenta-acta">
                                 Asambleistas
                             </Link>
                     </div>
